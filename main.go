b9a32accb3112096de9b16d194df1fc455249e84
Muhammad Reza Zulman <m.rezazulman@gmail.com>
2022-11-16 23:52:12 +0700

    fix pipeline 4

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
+	"strconv"
 
 	"peerac/go-sys/model"
 	"peerac/go-sys/utils"
@@ -29,6 +31,23 @@ func main() {
 
 }
 
+var (
+	suffixes [5]string
+)
+
+func HumanFileSize(size float64) string {
+	suffixes[0] = "B"
+	suffixes[1] = "KB"
+	suffixes[2] = "MB"
+	suffixes[3] = "GB"
+	suffixes[4] = "TB"
+
+	base := math.Log(size) / math.Log(1024)
+	getSize := utils.Round(math.Pow(1024, base-math.Floor(base)), .5, 2)
+	getSuffix := suffixes[int(math.Floor(base))]
+	return strconv.FormatFloat(getSize, 'f', -1, 64) + " " + string(getSuffix)
+}
+
 func SysInfoHandler(c *gin.Context) {
 	// Get Host Information
 	hostInfo, _ := host.Info()
@@ -47,11 +66,11 @@ func SysInfoHandler(c *gin.Context) {
 	fmt.Println(memInfo.Used)
 
 	memData := model.RAM{
-		Total:       utils.HumanFileSize(float64(memInfo.Total)),
-		Available:   utils.HumanFileSize(float64(memInfo.Available)),
-		Used:        utils.HumanFileSize(float64(memInfo.Used)),
+		Total:       HumanFileSize(float64(memInfo.Total)),
+		Available:   HumanFileSize(float64(memInfo.Available)),
+		Used:        HumanFileSize(float64(memInfo.Used)),
 		UsedPercent: memInfo.UsedPercent,
-		Free:        utils.HumanFileSize(float64(memInfo.Free)),
+		Free:        HumanFileSize(float64(memInfo.Free)),
 	}
 
 	// Populate response
--- utils/file_size.go
+++ utils/file_size.go
@@ -1,13 +1,7 @@
 package utils
 
 import (
-	"fmt"
 	"math"
-	"strconv"
-)
-
-var (
-	suffixes [5]string
 )
 
 func Round(val float64, roundOn float64, places int) (newVal float64) {
@@ -22,18 +16,4 @@ func Round(val float64, roundOn float64, places int) (newVal float64) {
 	}
 	newVal = round / pow
 	return
-}
-
-func HumanFileSize(size float64) string {
-	fmt.Println(size)
-	suffixes[0] = "B"
-	suffixes[1] = "KB"
-	suffixes[2] = "MB"
-	suffixes[3] = "GB"
-	suffixes[4] = "TB"
-
-	base := math.Log(size) / math.Log(1024)
-	getSize := Round(math.Pow(1024, base-math.Floor(base)), .5, 2)
-	getSuffix := suffixes[int(math.Floor(base))]
-	return strconv.FormatFloat(getSize, 'f', -1, 64) + " " + string(getSuffix)
 }
